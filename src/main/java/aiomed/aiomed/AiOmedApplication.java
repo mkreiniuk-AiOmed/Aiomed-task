8b8550054755f7034419bf3876a37e898a33deca
Mark Kreiniuk <markk@noviopus.com>
2024-03-07 00:05:40 +0200

    Initial commit

---
+++ src/main/java/aiomed/aiomed/AiOmedApplication.java
@@ -0,0 +1,13 @@
+package aiomed.aiomed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiOmedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aiomed/aiomed/AiOmedApplicationTests.java
@@ -0,0 +1,13 @@
+package aiomed.aiomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiOmedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
